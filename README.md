439a6975ab0a9a8e9894d704571849bccd612419
Danielle Stone Awounang <stonetemfack@gmail.com>
2023-03-22 04:45:06 +0100

    creation d'un autre projet

--- README.md
+++
@@ -1,1 +0,0 @@
-# Notes_approche_deux
--- app/src/main/java/com/example/notesapp/feature_note/presentation/add_edit_note/components/NoteTextFieldState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.notesapp.feature_note.presentation.add_edit_note.components
-
-import androidx.compose.runtime.State
-
-data class NoteTextFieldState(
-    val text: String = "",
-    val hint:String = "",
-    val isHintVisible : Boolean = true
-)
